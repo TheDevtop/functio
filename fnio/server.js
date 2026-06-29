884e596247c187c26d0876d52da1579612396810
Thijs Haker <theage2001@gmail.com>
2026-06-29 13:34:14 +0200

    Quickly implemented server post function

--- fnio/server.js
+++ fnio/server.js
@@ -4,14 +4,27 @@
 */
 
 const express = require('express');
+const sched = require('./sched');
 
 const appRouter = express();
-var srv;
+let srv;
 
-appRouter.get('/', (req, res) => {
-  res.send('Fn(io)');
+appRouter.use(express.json());
+
+appRouter.get('/health', (req, res) => {
+    res.send('Fn(io)');
 });
 
+appRouter.post('/', (req, res) => {
+    let result = null;
+    if (req.body.length > 1) {
+        const args = req.body.slice(1);
+        result = await sched.execNow(req.body[0], ...args);
+    } else {
+        result = await sched.execNow(req.body[0]);
+    }
+    res.json(result);
+});
 
 exports.startServer = (port) => {
     srv = appRouter.listen(port, () => {
